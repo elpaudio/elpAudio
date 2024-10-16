d749e887c0ebf318a4f5971e8e0bcf74a2ef0d69
elpoep <elpoepstudios@gmail.com>
2024-10-17 00:01:34 +0300

    build zip files tool

---
+++ elpAudio/build.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+del elpaudio.zip
+"c:\program files\7-zip\7z.exe" a "elpaudio.zip" LICENSE.txt music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.m3u -xr!*.m3u8 -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
+
+del elpaudio-nochromakey.zip
+"c:\program files\7-zip\7z.exe" a "elpaudio-nochromakey.zip" LICENSE.txt music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio-nochromakey.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
+
+ECHO DONE
+PAUSE
