35a2de880b7c025fd616d16909c79ae081dcd799
elpoep <elpoepstudios@gmail.com>
2024-10-22 02:02:32 +0300

    add data\ico.ico

--- elpAudio/build.bat
+++ elpAudio/build.bat
@@ -10,11 +10,11 @@ del elpaudio-nochromakey.zip
 
 echo Building original build with WINAPI2
 del elpaudio-winapi.zip
-"c:\program files\7-zip\7z.exe" a "elpaudio-winapi.zip" LICENSE.txt windows.dll music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.m3u -xr!*.m3u8 -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
+"c:\program files\7-zip\7z.exe" a "elpaudio-winapi.zip" data\ico.ico LICENSE.txt windows.dll music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.m3u -xr!*.m3u8 -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
 
 echo Building no-chromakey build with WINAPI2
 del elpaudio-nochromakey-winapi.zip
-"c:\program files\7-zip\7z.exe" a "elpaudio-nochromakey-winapi.zip" LICENSE.txt windows.dll music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio-nochromakey.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
+"c:\program files\7-zip\7z.exe" a "elpaudio-nochromakey-winapi.zip" data\ico.ico LICENSE.txt windows.dll music_examples\ themes\tahoma.ttf data\elpAudioSettings.exe data\vars.txt elpaudio-nochromakey.exe visualisers\default_*.vis lib_audio.dll fmodex.dll README.txt -xr!*.zip plugins\README.txt plugins\test.plg plugins\ea_monitors.dll plugins\monitor.plg themes\default themes\light themes\compact themes\readme.txt -xr!themes\*\playlist\ 
 
 
 ECHO DONE
